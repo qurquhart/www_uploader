8476eefd0cdc4032f8985a303f16bb523736a1f3
Qurquhart <quintonurquhart@gmail.com>
2020-07-16 00:20:32 -0700

    argparse discovery

--- uploader.py
+++ uploader.py
@@ -5,4 +5,14 @@
 # copy files to directory
 # backup selection options and restoration (creates backup of current files first for an undo)
 
-# logging
+# logging 
+
+import argparse
+
+parser = argparse.ArgumentParser(description="test boi")
+
+parser.add_argument("echo", help="help string here")
+
+arguments = parser.parse_args()
+
+print(arguments.echo)
