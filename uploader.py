f4dbf9b0dd7ab08e1184b296e4ca6bce4311f51f
Qurquhart <quintonurquhart@gmail.com>
2020-07-16 00:06:06 -0700

    notes

---
+++ uploader.py
@@ -0,0 +1,8 @@
+# ftp utility
+# credential solution
+# filepath of www
+# move current files to backup directory eg (f"backup/{datetime.now()}")
+# copy files to directory
+# backup selection options and restoration (creates backup of current files first for an undo)
+
+# logging
